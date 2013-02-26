ee58f5bbd99df797119b075947854037352a2307
Klas Jönsson <klas.joensson@gmail.com>
2013-02-26 10:24:18 +0100

    Adds a logger and documentation to the TestLogin-class

--- plugins/net.bioclipse.opentox/src/net/bioclipse/opentox/business/TestLogin.java
+++ plugins/net.bioclipse.opentox/src/net/bioclipse/opentox/business/TestLogin.java
@@ -5,13 +5,28 @@ import java.util.HashMap;
 import net.bioclipse.opentox.OpenToxLogInOutListener;
 import net.bioclipse.usermanager.ITestAccountLogin;
 
+import org.apache.log4j.Logger;
 import org.opentox.aa.opensso.OpenSSOToken;
 
+/**
+ * This class tests if its possibly to log in to OpenTox with the properties
+ * provided in a hash map.
+ * 
+ * @author klasjonsson
+ *
+ */
 public class TestLogin implements ITestAccountLogin {
+    
+    private Logger logger = Logger.getLogger( this.getClass() );
     
     public TestLogin() {    }
     
+    /**
+     * Tries to login and if is succeeded it logs out immediately.
+     * @param A hash map containing the properties that are to be tested 
+     */
     public boolean login(HashMap<String, String> myProperites) {
+        /* TODO The key-values should be obtained from plugin.xml, if possibly*/
         String authService = myProperites.get( "auth. service" );
         String userName = myProperites.get( "username" );
         String pwd = myProperites.get( "password" );
@@ -26,24 +41,32 @@ public class TestLogin implements ITestAccountLogin {
             if (succsess)
                 token.logout();
         } catch ( Exception e ) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            logger.error( "Something went wrong when trying to into OpenTox:" +
+                    e.getMessage() );
         }
         
         return succsess;
     }
 
+    /**
+     * A method for identify the test-class. I.e. it returns the name of the 
+     * plug-in.
+     * 
+     * @return The name of the plug-in, i.e. "OpenTox"
+     */
     @Override
     public String getAccountType() {
         try {
-            OpenToxLogInOutListener listener = OpenToxLogInOutListener.getInstance();
+            OpenToxLogInOutListener listener = OpenToxLogInOutListener
+                    .getInstance();
             return listener.getAccountType();
         } catch ( InstantiationException e ) {
             /* If we end up here the listener hasn't been instantiated yet, 
              * but this is done when the plug in is registered in the usermanger
              * => if not done when calling this method then something is done 
              * wrong some where else....*/
-            e.printStackTrace();
+            logger.error( "Tried to log in to OpenTox before the listener" +
+            		" was instantiated, should be impossibly: "+e.getMessage());
         }
 
         return null;
